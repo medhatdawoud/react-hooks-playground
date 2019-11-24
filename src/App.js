26e921b50e3b3ba0d1b6c0a0364ec3841ada7a6f
Medhat Dawoud <medhatdawoud@gmail.com>
2019-11-24 16:25:53 +0100

    Initial component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
+
+  countUp = () => {
+    this.setState({ counter: ++this.state.counter });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.countUp}>Count up</button>
+        <div style={{ fontSize: '50px', textAlign: 'center' }}>
+          {this.state.counter}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
