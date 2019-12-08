2c840371389fced59762944bd249f87430825cc8
Medhat Dawoud <medhatdawoud@gmail.com>
2019-12-08 21:31:11 +0100

    Code of episode 3: useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function App () {
-  const [counter, setCounter] = React.useState(0)
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [title, setTitle] = useState('');
+
+  const handleCounter = () => setCounter((c) => c + 1);
+  const handelTitle = () => setTitle('semiColon academy');
+
+  useEffect(() => {
+    console.log('inside useEffect 1');
+    document.title = title;
+
+    return () => {
+      setTimeout(() => {
+        setTitle('');
+        console.log('cleanup');
+      }, 1000);
+    };
+  }, [title]);
+
+  useEffect(() => {
+    console.log('inside useEffect 2');
+    document.title = `You have clicked ${counter} times`;
+  }, [counter]);
 
   return (
     <div className="container text-center pt-5">
-      <button
-        className="btn btn-primary"
-        onClick={() => setCounter(c => c + 1)}
-      >
+      <button className="btn btn-primary" onClick={handleCounter}>
         Count up
+      </button>
+      <button className="btn btn-primary" onClick={handelTitle}>
+        Change title
       </button>
       <h1 className="mt-3">{counter}</h1>
     </div>
-  )
+  );
 }
--- src/App_class.js
+++ src/App_class.js
@@ -6,13 +6,30 @@ export default class App extends Component {
 
     this.state = {
       counter: 0,
+      title: '',
     };
 
     this.countUp = this.countUp.bind(this);
+    this.handleTitle = this.handleTitle.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('mounted');
+  }
+
+  componentDidUpdate() {
+    console.log('updated');
+    document.title = this.state.title;
+
+    document.title = `You have clicked ${this.state.counter} times`;
   }
 
   countUp() {
     this.setState({ counter: this.state.counter + 1 });
+  }
+
+  handleTitle() {
+    this.setState({ title: 'semicolon academy' });
   }
 
   render() {
@@ -20,6 +37,9 @@ export default class App extends Component {
       <div className="container text-center pt-5">
         <button className="btn btn-primary" onClick={this.countUp}>
           Count up
+        </button>
+        <button className="btn btn-primary" onClick={this.handleTitle}>
+          change Title
         </button>
         <h1 className="mt-3">{this.state.counter}</h1>
       </div>
