88502196c95aa857cec9b0cd5cf3e4966261a099
Medhat Dawoud <medhatdawoud@gmail.com>
2020-01-14 21:37:23 +0100

    Fix padding in button component

--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,6 @@
 import React from 'react';
 
 export const Button = React.memo(({ onClick, label, n }) => {
-
   return (
     <button className="btn btn-primary mt-3" onClick={() => onClick(n)}>
       {label}
